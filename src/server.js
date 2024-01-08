4ba29e5e85a249255c5c75a49973626f728a6f49
Paul Delgado <bryand9970@gmail.com>
2024-01-08 16:39:30 -0500

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,31 @@
+// Requerir los módulos
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+import express from 'express'
+import cors from 'cors'
+import routerVeterinarios from './routers/veterinario_routes.js'
+
+// Inicializaciones
+const app = express();
+
+// Configuraciones 
+app.set('port',process.env.port || 3000)
+app.use(cors())
+
+// Middlewares 
+app.use(express.json())
+
+
+// Variables globales
+
+
+
+// Rutas 
+app.use('/api',routerVeterinarios)
+// Manejo de una ruta que no sea encontrada
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+
+
+// Exportar la instancia de express por medio de app
+export default app
