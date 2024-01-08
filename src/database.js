cda4d4401e204c259194375908e660684ca29b3a
Paul Delgado <bryand9970@gmail.com>
2024-01-08 16:39:37 -0500

    Create database.js

---
+++ src/database.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const mongo_uri = "mongodb://0.0.0.0:27017/vet"
+
+mongoose.set('strictQuery', true)   
+const connection = async()=>{
+    try {
+        const {connection} = await mongoose.connect(mongo_uri)
+        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default  connection
