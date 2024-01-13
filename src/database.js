08ce09a5f2fcf8bf7668e538222028bb6f87fcd1
Bryan Delgado <bryand9970@gmail.com>
2024-01-12 19:01:41 -0500

    Update string connection

--- src/database.js
+++ src/database.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose'
 
-const mongo_uri = "mongodb://0.0.0.0:27017/vet"
-
 mongoose.set('strictQuery', true)   
 const connection = async()=>{
     try {
-        const {connection} = await mongoose.connect(mongo_uri)
+        const {connection} = await mongoose.connect(process.env.COMPASS_URI /*&& process.env.ATLAS_URI*/)
         console.log(`Database is connected on ${connection.host} - ${connection.port}`)
     } catch (error) {
         console.log(error);
