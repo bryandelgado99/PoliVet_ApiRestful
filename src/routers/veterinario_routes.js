833868890410bb3247855bc5d9bd51250b5d26ea
Bryan Delgado <bryand9970@gmail.com>
2024-01-12 19:02:05 -0500

    Veterinario Routes an controller

--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -2,28 +2,6 @@ import {Router} from 'express'
 
 const router = Router()
 
-/*outer.post('/login',(req,res)=>res.send("login"))
-
-router.post('/registro',(req,res)=>res.send("registro"))
-
-router.get('/confirmar/:token',(req,res)=>res.send("confirmar email"))
-
-router.get('/veterinarios',(req,res)=>res.send("lista de veterinarios"))
-
-router.get('/recuperar-password',(req,res)=>res.send("enviar mail"))
-
-router.get('/recuperar-password/:token',(req,res)=>res.send("verificar token"))
-
-router.post('/nuevo-password/:token',(req,res)=>res.send("crear password"))
-
-router.get('/perfil',(req,res)=>res.send("perfil"))
-
-router.put('/veterinario/actualizarpassword',(req,res)=>res.send("actualizar password"))
-
-router.get('/veterinario/:id',(req,res)=>res.send("detalle del veterinario"))
-
-router.put('/veterinario/:id',(req,res)=>res.send("actualizar perfil"))*/
-
 import {
     login,
     perfil,
@@ -42,10 +20,9 @@ router.post("/login", login);
 router.post("/registro", registro);
 router.get("/confirmar/:token", confirmEmail);
 router.get("/veterinarios", listarVeterinarios);
-router.get("/recuperar-password", recuperarPassword);
+router.post("/recuperar-password", recuperarPassword);
 router.get("/recuperar-password/:token", comprobarTokenPasword);
 router.post("/nuevo-password/:token", nuevoPassword);
-
 router.get("/perfil", perfil);
 router.put('/veterinario/actualizarpassword',actualizarPassword)
 router.get("/veterinario/:id", detalleVeterinario);
