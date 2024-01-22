4d7852366e1d9a8d471e1d551256ada5bee8da5b
Paul Delgado <bryand9970@gmail.com>
2024-01-22 15:37:51 -0500

    Update Server

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ app.use(express.json())
 // Rutas 
 app.use('/api',routerVeterinarios)
 app.use('/api',routerPacientes)
+
 // Manejo de una ruta que no sea encontrada
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
